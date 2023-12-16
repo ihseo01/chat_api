ba49c7880cfbe030daace0082a958bc8fd48187c
ihseo01 <ihseo@beyondmedicine.kr>
2023-12-16 18:40:36 +0900

    feat: chat 기능 추가

--- src/main/java/com/study/chatting/api/controller/MessageController.java
+++ src/main/java/com/study/chatting/api/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.study.chatting.api.controller;
 
+import com.study.chatting.api.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -9,4 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageController {
     private final SimpMessageSendingOperations sendingOperations;
+
+    @MessageMapping("/chat/message")
+    public void enter(ChatMessage message) {
+        if(ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        }
+        sendingOperations.convertAndSend("/pub/room/" + message.getRoomId(), message);
+    }
 }
